edebf6908463555052ceccaea7d3952f565caa7c
trist <tristan@lataniere.net>
2023-03-21 23:42:48 +0100

    test of queries

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import pysmashgg
-import self as self
 import json
 
 
 def print_hi(name):
     print(f'Hi, {name}')
 
-
 if __name__ == '__main__':
     print_hi('PyCharm')
     with open('TOKEN_STARTGG.txt', 'r') as file:
         token = file.read().rstrip()
-        print(token)
     smash = pysmashgg.SmashGG(token, True)
     # Shows a complete list of bracket ids given tournament and event names
-    brackets = smash.tournament_show_event_brackets("let-s-play-ii", "1v1-smash-ultimate")
-    print(brackets)
-    print(brackets['bracketIds'][0])
-    idBracket = brackets['bracketIds'][0]
-    sets = smash.tournament_show_sets("let-s-play-ii", "1v1-smash-ultimate", 1)
-    print(sets)
-    for set in sets:
-        print(set)
+    # brackets = smash.tournament_show_event_brackets("let-s-play-ii", "1v1-smash-ultimate")
+    # print(brackets)
+    # print(brackets['bracketIds'][0])
+    # idBracket = brackets['bracketIds'][0]
+
+    #sets = smash.tournament_show_sets("fight-night-73", "smash-ultimate-singles", 0)
+    #print(sets)
+    for i in range(1,10):
+        sets = smash.tournament_show_sets("let-s-play-ii", "1v1-smash-ultimate", i)
+
+        for set in sets:
+            print(set['fullRoundText'] + "  player 1: " + set['entrant1Name'] + "  player 2: " + set['entrant2Name'])
+            #print(set)
 
     print(len(sets))
+
 
     # parsed = json.loads(sets)
     # print(json.dumps(parsed, indent=4))
