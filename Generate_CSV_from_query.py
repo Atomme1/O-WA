6a376b038590a19aa34bb09a920d2452d2b1ec1c
trist <tristan@lataniere.net>
2023-03-22 14:08:47 +0100

    Query + csv generated

---
+++ Generate_CSV_from_query.py
@@ -0,0 +1,52 @@
+import pysmashgg
+import json
+import pandas as pd
+
+
+# This code allows you to generate a CSV from pysmashgg functions query and use this CSV inside the ScoreBoard App,
+# it will go through pages, more recent being 0 or 1 to n number of matchs)
+# For example if your tournament has 64 players you will have 32 WinnerRound1 sets to played and the same number for
+# LoserRound1.
+# The variable "perPage" inside the function and the query made by pysmashgg allows to retrieve only 18 sets
+# (if you want more like 37 as I did, go in the code and wiggle around the query and test the limit)
+# Do quick a math of how many matches is there to be played and the number of pages you will input.
+# 18*2 = 36 sets so you range must be 2 if you want all the matches from WinnerRound1
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    with open('TOKEN_STARTGG.txt', 'r') as file:
+        token = file.read().rstrip()
+    smash = pysmashgg.SmashGG(token, True)
+
+    fullSetsInTournamentEvent = []
+    fullTextRound = []
+    fullEntrant1Name = []
+    fullEntrant2Name = []
+    for i in range(0, 10):
+        sets = smash.tournament_show_sets("let-s-play-ii", "1v1-smash-ultimate", i)
+
+        for set in sets:
+            print(set['fullRoundText'] + "  player 1: " + set['entrant1Name'] + "  player 2: " + set['entrant2Name'])
+            fullSetsInTournamentEvent.append(set)
+            fullTextRound.append(set['fullRoundText'])
+            fullEntrant1Name.append(set['entrant1Name'])
+            fullEntrant2Name.append(set['entrant2Name'])
+    print(len(fullSetsInTournamentEvent))
+
+    data_lp = {"Time": fullTextRound, "HomeTeam": fullEntrant1Name, "AwayTeam": fullEntrant2Name}
+    df_data_lp = pd.DataFrame(data_lp)
+    df_data_lp.to_csv("data_lp.csv", sep=';', encoding='utf-8', index=False)
+    # parsed = json.loads(sets)
+    # print(json.dumps(parsed, indent=4))
+
+# {
+#   "phaseId": 1308067,
+#   "page": 1,
+#   "perPage": 15,
+#   "tournamentSlug": "let-s-play-ii",
+#   "eventSlug": "1v1-smash-ultimate"
+# }
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # StartGG_queries
-StartGG queries for my association USDEM 404.
+StartGG queries for my association USDEM ESPORT 404.
 
 ## Useful links
 
- The main librairy used is PySmashGG : https://pypi.org/project/pysmashgg/
+The main librairy used is PySmashGG :https://pypi.org/project/pysmashgg/
+
+The external software I use for keeping up with the score of the players: 
+https://obsproject.com/forum/resources/another-scoreboard-application.827/
+This simple scoreboard application allows me to load a CSV of the matches to be played.
+With this 
